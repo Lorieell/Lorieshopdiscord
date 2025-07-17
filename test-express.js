694628e6856777d0a392f89c124afd4d723d8ad6
Lorieell <soidikminaxio@gmail.com>
2025-07-18 00:54:15 +0200

    Remove unnecessary filedel carts.json

--- test-express.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Test: Bot is alive!');
-});
-app.listen(3000, '127.0.0.1', () => {
-  console.log('Test app is listening on port 3000');
-});
